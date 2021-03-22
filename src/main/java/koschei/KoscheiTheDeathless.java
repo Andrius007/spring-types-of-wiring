4dc40fab6127f163075a5fd6e5f08bda8692e86b
Andrius007 <79835280+Andrius007@users.noreply.github.com>
2021-03-22 23:54:22 +0300

    Решение на проверку

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,20 +1,22 @@
 package koschei;
 
 import koschei.models.Ocean1;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KoscheiTheDeathless {
 
-    private Ocean1 ocean;
+    private final Ocean1 ocean;
 
-    public String getRulesByDeth() {
+    public KoscheiTheDeathless(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+
+
+    public String getRulesByDeath() {
+
         return "На свете есть океан , " + ocean.toString();
     }
 
-    @Autowired
-    public void setOcean(Ocean1 ocean) {
-        this.ocean = ocean;
-    }
+
 }
